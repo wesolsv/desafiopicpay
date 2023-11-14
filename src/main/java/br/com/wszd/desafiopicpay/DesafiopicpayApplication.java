3e7eb6b352c7915984ab0ebd301cb9b2b5571553
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-11-14 16:05:13 -0300

    feat: initial config

---
+++ src/main/java/br/com/wszd/desafiopicpay/DesafiopicpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.wszd.desafiopicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiopicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiopicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wszd/desafiopicpay/DesafiopicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wszd.desafiopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiopicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
